8977697e35a0db2fcf1996eebf30701133e3e025
Amodh Dhakal <adhakal2@ncsu.edu>
2025-01-16 18:06:33 -0500

    Simplified code

--- src/main.ts
+++ src/main.ts
@@ -16,30 +16,24 @@ import {
 } from "@babylonjs/core";
 
 import BG_URL from "/BG.png";
-import SPACESHIP_URL from "/spaceship.png"
 
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const engine = new Engine(canvas, true);
 const scene = new Scene(engine);
 
-const layer = new Layer("Background", BG_URL, scene);
-
+new Layer("Background", BG_URL, scene);
 const camera = new UniversalCamera("Camera", new Vector3(0, 0, -5), scene);
+camera.inputs.clear();
 camera.attachControl(canvas, true);
 
-new HemisphericLight("lightUp", new Vector3(0, 1, 0), scene);
-new HemisphericLight("lightDown", new Vector3(0, 0, 0), scene);
-const spaceship = MeshBuilder.CreateBox("spaceship", { width: 0.5, height: 0.5 });
+//? Just for testing
+MeshBuilder.CreateSphere("");
 
 window.addEventListener("keydown", (ev) => {
-  // ctrl-d
-  if (ev.ctrlKey && ev.key === "d") {
-    if (scene.debugLayer.isVisible()) {
-      scene.debugLayer.hide();
-    } else {
-      scene.debugLayer.show();
-    }
-  }
+  if (!ev.ctrlKey || ev.key !== "d") return;
+
+  //* Ctrl-D for debug
+  scene.debugLayer.isVisible() ? scene.debugLayer.hide() : scene.debugLayer.show();
 });
 
 engine.runRenderLoop(() => {
